be6efb955428876cb19aaaac89f4fba325135eac
kperkins97 <perkins.kelsey@yahoo.com>
2021-09-19 13:40:22 -0500

    still trying to loop questions

--- javascript/script.js
+++ javascript/script.js
@@ -4,9 +4,10 @@ var timerEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var ptagEl = document.getElementById("pTag");
 var answerEl = document.querySelectorAll(".answer");
-//create endgame function here. Setting it equal to clearInterval(timerInterval)
-function endGame (){
-}
+// //create endgame function here. Setting it equal to clearInterval(timerInterval)
+// function endGame (){
+    
+// }
 
 function startTimer() {
     var timerInterval = setInterval(function() {
@@ -20,21 +21,46 @@ function startTimer() {
 }
 startTimer();
 
-ptagEl.textContent = "The Correct Braces For A JavaScript Array is [ ]"; 
+ptagEl.textContent = "The Correct Braces For A JavaScript Array is [ ]";
 answerEl[0].textContent = "Ture";
 answerEl[1].textContent = "False";
 
 // changing the text of pTag so second question can display 
 
-ptagEl.textContent = "Can You Use CSS Inside JavaScript?";
-answerEl[0].textContent = "No";
-answerEl[1].textContent = "Yes";
+// ptagEl.textContent = "Can You Use CSS Inside JavaScript?";
+// answerEl[0].textContent = "Yes";
+// answerEl[1].textContent = "No";
+
 // changing the text of pTag so third question can display 
-ptagEl.textContent = "Variables Can NOT Be Re-Assigned Multiple Values";
-answerEl[0].textContent = "Ture";
-answerEl[1].textContent = "False";
+// ptagEl.textContent = "Variables Can NOT Be Re-Assigned Multiple Values";
+// answerEl[0].textContent = "False";
+// answerEl[1].textContent = "True";
 
+var currentQuestionCount = 0;
+var text= "";
 
+function getNextQuestion() {
+    currentQuestionCount++;
+    //need to loop the two other questions here
+    ptagEl.textContent = "Can You Use CSS Inside JavaScript?";
+    answerEl[0].textContent = "Yes";
+    answerEl[1].textContent = "No";
+    // for (var i = 0, i < 3) trying loop something here...maybe for in or for each? because I 
+
+}
+answerEl[0].addEventListener('click', function() {
+    console.log('button one was clicked');
+    // button one will always be correct answer
+    getNextQuestion();
+});
+
+answerEl[1].addEventListener("click", function() {
+    console.log("button two was clicked")
+    // buttone two will always be incorrect answer
+    getNextQuestion();
+});
+
+// this 
 //  const quizQuestions = [   
     // {
     //   prompt: "Select The Correct Braces For An Array:\n(a) [ ]\n\ (b) { }\n(c) ( )",
@@ -86,7 +112,7 @@ answerEl[1].textContent = "False";
 // }
 
 function displayMessage() {
-    timerEl.textContent = ('');
+    timerEl.textContent = ('Times Up!');
 }
 
 startTimer();
