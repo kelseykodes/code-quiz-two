082dab148d5811229065efd9065b37726fa26a1b
kperkins97 <perkins.kelsey@yahoo.com>
2021-09-20 16:25:20 -0500

    trying get start button to work

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,17 @@ var timerEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var ptagEl = document.getElementById("pTag");
 var answerEl = document.querySelectorAll(".answer");
-// //create endgame function here. Setting it equal to clearInterval(timerInterval)
+var startBtn = document.getElementById('start-button')
+var card = document.querySelector(".card")
+// //create endgame function here. Setting it equal to clearInterval(timerInterval) so if user finishes before timer is up, the timer stops.
 // function endGame (){
-    
 // }
+
+startBtn.addEventListener("click", function(){
+    console.log("start button was clicked")
+    card.setAttribute("style", "visibility: visible;");
+
+});
 
 function startTimer() {
     var timerInterval = setInterval(function() {
@@ -22,19 +29,20 @@ function startTimer() {
 startTimer();
 
 ptagEl.textContent = "The Correct Braces For A JavaScript Array is [ ]";
-answerEl[0].textContent = "Ture";
+answerEl[0].textContent = "True";
 answerEl[1].textContent = "False";
+answerEl[2].textContent = "not an answer option"
+answerEl[3].textContent = "not an answer option"
+// // changing the text of pTag so second question can display 
 
-// changing the text of pTag so second question can display 
+// // ptagEl.textContent = "Can You Use CSS Inside JavaScript?";
+// // answerEl[0].textContent = "Yes";
+// // answerEl[1].textContent = "No";
 
-// ptagEl.textContent = "Can You Use CSS Inside JavaScript?";
-// answerEl[0].textContent = "Yes";
-// answerEl[1].textContent = "No";
-
-// changing the text of pTag so third question can display 
-// ptagEl.textContent = "Variables Can NOT Be Re-Assigned Multiple Values";
-// answerEl[0].textContent = "False";
-// answerEl[1].textContent = "True";
+// // changing the text of pTag so third question can display 
+// // ptagEl.textContent = "Variables Can NOT Be Re-Assigned Multiple Values";
+// // answerEl[0].textContent = "False";
+// // answerEl[1].textContent = "True";
 
 var currentQuestionCount = 0;
 var text= "";
@@ -43,11 +51,15 @@ function getNextQuestion() {
     currentQuestionCount++;
     //need to loop the two other questions here
     ptagEl.textContent = "Can You Use CSS Inside JavaScript?";
-    answerEl[0].textContent = "Yes";
-    answerEl[1].textContent = "No";
+    console.log(ptagEl);
+    answerEl[0].textContent = "not an answer option"
+    answerEl[1].textContent = "not an answer option"
+    answerEl[2].textContent = "Yes";
+    answerEl[3].textContent = "No";
+    //answerEl[2].textContent = "click to see last question
     // for (var i = 0, i < 3) trying loop something here...maybe for in or for each? because I 
-
 }
+
 answerEl[0].addEventListener('click', function() {
     console.log('button one was clicked');
     // button one will always be correct answer
@@ -60,19 +72,43 @@ answerEl[1].addEventListener("click", function() {
     getNextQuestion();
 });
 
-// this 
+function getLastQuestion() {
+    currentQuestionCount++;
+    ptagEl.textContent = "Variables Can NOT Be Re-Assigned Multiple Values";
+    console.log(ptagEl);
+    answerEl[0].textContent = "not an answer option"
+    answerEl[1].textContent = "not an answer option"
+    answerEl[2].textContent = "False";
+    answerEl[3].textContent = "True";
+};
+answerEl[2].addEventListener('click', function() {
+    console.log('button one was clicked');
+    // button one will always be correct answer
+    getLastQuestion();
+});
+
+answerEl[3].addEventListener("click", function() {
+    console.log("button two was clicked")
+    // buttone two will always be incorrect answer
+    getLastQuestion();
+});
+
+
 //  const quizQuestions = [   
-    // {
-    //   prompt: "Select The Correct Braces For An Array:\n(a) [ ]\n\ (b) { }\n(c) ( )",
-    //    answer: "a"
+//     {
+//       prompt:  "The Correct Braces For A JavaScript Array is [ ]",
+//       choices: ["True", "False"],
+//       answer: "True"
 //   },
 //   {
-    //   prompt: "Can You Style CSS In JavaScript?\n(a) No\n\ (b) Yes",
-    //   answer: "b"
+//       prompt: "Can You Use CSS Inside JavaScript?",
+//       choices: ["Yes", "No"],
+//       answer: "Yes"
 //   },
 //    {
-    // prompt: "Variables Can NOT Be Re-Assigned Multiple Values:\n(a) False\n\ (b) True",
-    // answer: "a"
+//     prompt: "Variables Can NOT Be Re-Assigned Multiple Values.",
+//     chocies: ["False", "True",],
+//     answer: "False"
 //    },
 // ];
 
@@ -116,3 +152,11 @@ function displayMessage() {
 }
 
 startTimer();
+
+
+// the form to store the users to store info 
+// headTitle.textContent = "The quiz is now over!"
+// ques.textContent = "Your final score was " + secondsLeft;
+// var endMess = document.createElement('form');
+// newItem.innerHTML = "<label> Name:</label><br>\n<input type='text'<br><br>\n<a href= './/highscore.html'> <input type='submit' value='Submit'> </a>";
+// replaceAns.parentNode.replaceChild(endMess, replaceAns);
